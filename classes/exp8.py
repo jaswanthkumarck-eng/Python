3eaee63051fa34398b41f0a60a409195558a16a7
Jaswanthkumar CK <jaswanthkumar.ck@gmail.com>
2026-07-04 12:02:39 +0530

    Added classes folder

---
+++ classes/exp8.py
@@ -0,0 +1,12 @@
+class phone():
+    def __init__(self,brand,price,chargertype):
+        self.brand=brand
+        self.price=price
+        self.chargertype=chargertype
+    def display(self):
+        print("brand:",self.brand)
+        print("price:",self.price)
+        print("chargertype:",self.chargertype)
+
+samsung=phone("Samsung",20000,"Type C")
+samsung.display()
