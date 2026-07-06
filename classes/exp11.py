d2da2c7a1af66cb774a0c674ea1f8053e4788eca
Jaswanthkumar CK <jaswanthkumar.ck@gmail.com>
2026-07-06 22:53:15 +0530

    Added classes folder

--- classes/exp11.py
+++ classes/exp11.py
@@ -2,10 +2,15 @@ class dad():
     def phone(self):
         print("dads phone")
 
-class son(dad):
+class mom():
+    def sweet(self):
+        print("moms sweet")
+
+class son(dad,mom):
     def laptop(self):
         print("sons laptop")
 
 ram=son()
 ram.phone()
 ram.laptop()
+ram.sweet()
