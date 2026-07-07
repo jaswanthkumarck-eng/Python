ff78958ffb8e8facdd7415177b7bfbcac40e1a6e
Jaswanthkumar CK <jaswanthkumar.ck@gmail.com>
2026-07-07 22:15:38 +0530

    Added classes folder

---
+++ classes/exp14.py
@@ -0,0 +1,17 @@
+class a():
+
+    def  __init__(self):
+        print("a")
+        
+    def display(self):
+        print("you are in class a")
+    
+class b():
+    def  __init__(self):
+        print("b")
+        
+    def display(self):
+        print("you are in class b")
+
+ob1=a()
+ob2=b()
