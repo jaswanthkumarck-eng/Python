d52205e4d1eecbfc5714578ae7552495c4ad8a43
Jaswanthkumar CK <jaswanthkumar.ck@gmail.com>
2026-07-07 22:18:26 +0530

    Added classes folder

---
+++ classes/exp15.py
@@ -0,0 +1,25 @@
+class a():
+
+    def  __init__(self):
+        print("a")
+        
+    def display(self):
+        print("you are in class a")
+    
+class b():
+    def  __init__(self):
+        super() .__init__()
+        print("b")
+    def display(self):
+        print("you are in class b")
+
+class c():
+    def  __init__(self):
+        super() .__init__()
+        print("c")
+        
+    def display(self):
+        print("you are in class c")
+
+ob1=a()
+ob2=b()
