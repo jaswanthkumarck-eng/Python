90a9447f0bde8aaa1b380ee32f79473b34f92782
Jaswanthkumar CK <jaswanthkumar.ck@gmail.com>
2026-07-08 21:43:34 +0530

    Added classes folder

--- classes/exp18.py
+++ classes/exp18.py
@@ -2,5 +2,12 @@ class animal():
     def sound(self):
         print("Animal makes a sound")
 
+class dog(animal):
+    def sound(self):
+        print("Dog barks")
+
+
 a1 = animal()
 a1.sound()
+d1 = dog()
+d1.sound()  
